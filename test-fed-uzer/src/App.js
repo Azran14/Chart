19aa5af3d08f48dcd002cd073565c2c64a1526f3
Azran14 <arnaud.dumarche@hetic.net>
2020-11-27 15:41:23 +0100

    responsive

--- test-fed-uzer/src/App.js
+++ test-fed-uzer/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Chart from "react-google-charts";
 import { useState } from "react";
+import icon from "../src/Logo.png";
 
 function App() {
   const [valueX, setValueX] = useState({});
@@ -13,42 +14,74 @@ function App() {
 
   return (
     <div>
-      <div>
-        <label>X</label>
-        <input
-          id="X"
-          type="number"
-          placeholder="ex : 7"
-          required
-          label="Required"
-          onChange={(e) => setValueX(e.target.value)}
-        />
-        <label>Y</label>
-        <input
-          id="Y"
-          type="number"
-          placeholder="ex : 5"
-          required
-          onChange={(e) => setValueY(e.target.value)}
-        />
-        <button onClick={handleClick}> poop</button>
+      <img className="logo" src={icon} alt="logo" />
+      <h1 className="title">Bienvenue sur votre espace Client</h1>
+      <div className="container">
+        <h2>Renseignez une valeur pour chaque champs</h2>
+        <div className="container__input">
+          <div className="container__input--X">
+            <label>Renseignez la valeur X</label>
+            <input
+              id="X"
+              type="number"
+              placeholder="ex : 7"
+              required
+              onChange={(e) => setValueX(e.target.value)}
+            />
+          </div>
+          <div className="container__input--Y">
+            <label>Renseignez la valeur Y</label>
+            <input
+              id="Y"
+              type="number"
+              placeholder="ex : 5"
+              required
+              onChange={(e) => setValueY(e.target.value)}
+            />
+          </div>
+        </div>
+        <button onClick={handleClick}>
+          <em>Visualiser</em>
+        </button>
+
+        {visible && (
+          <div className="container__chart">
+            <div className="container__chart--pie">
+              <Chart
+                width={"375px"}
+                height={"300px"}
+                chartType="PieChart"
+                loader={<div>Loading Chart</div>}
+                data={[
+                  ["Votre graphique", "En X et Y"],
+                  ["X", parseInt(valueX)],
+                  ["Y", parseInt(valueY)],
+                ]}
+                options={{
+                  title: "Votre graphique personnalisé (PieChart)",
+                }}
+              />
+            </div>
+            <div className="container__chart--donut">
+              <Chart
+                width={"800px"}
+                height={"500px"}
+                chartType="PieChart"
+                loader={<div>Loading Chart</div>}
+                data={[
+                  ["Votre graphique", "En X et Y"],
+                  ["X", parseInt(valueX)],
+                  ["Y", parseInt(valueY)],
+                ]}
+                options={{
+                  title: "Votre graphique personnalisé (DonutChart)",
+                  pieHole: 0.4,
+                }}
+              />
+            </div>
+          </div>
+        )}
       </div>
-      {visible && (
-        <Chart
-          width={"500px"}
-          height={"300px"}
-          chartType="PieChart"
-          loader={<div>Loading Chart</div>}
-          data={[
-            ["Your chart", "charterino"],
-            ["X", parseInt(valueX)],
-            ["Y", parseInt(valueY)],
-          ]}
-          options={{
-            title: "My Graph",
-          }}
-        />
-      )}
     </div>
   );
 }
